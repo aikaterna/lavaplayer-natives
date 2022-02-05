a5e50631585825db6f308af35be283010fb8f836
Stonedestroyer <1307729+Stonedestroyer@users.noreply.github.com>
2022-02-05 21:46:49 +0100

    Add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.aikaterna"
+version = "2.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
