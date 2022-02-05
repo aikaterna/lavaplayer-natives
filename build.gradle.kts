74c6829ef990d0ce0fe338e5e41e40277f578b84
Stonedestroyer <1307729+Stonedestroyer@users.noreply.github.com>
2022-02-06 00:21:04 +0100

    Publish maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,20 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
